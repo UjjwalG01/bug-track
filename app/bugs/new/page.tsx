eed9634662c44d7d0f8a168deefb9e87855e86c0
Ujjwal Giri <ug.online.st@gmail.com>
2023-10-16 15:00:29 +0545

    Form submission handled

--- app/bugs/new/page.tsx
+++ app/bugs/new/page.tsx
@@ -3,16 +3,38 @@
 import { Button, TextArea, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import { Controller, useForm } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+interface BugForm {
+  title: string;
+  description: string;
+}
 
 const NewBugPage = () => {
+  const { register, control, handleSubmit } = useForm<BugForm>();
+  const router = useRouter();
   return (
-    <div className="max-w-xl space-y-3">
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit((data) => {
+        axios.post("/api/bugs", data);
+        router.push("/bugs");
+      })}
+    >
       <TextField.Root>
-        <TextField.Input placeholder="Title" />
+        <TextField.Input placeholder="Title" {...register("title")} />
       </TextField.Root>
-      <SimpleMDE placeholder="Description" />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
       <Button>Add New Bug</Button>
-    </div>
+    </form>
   );
 };
 
