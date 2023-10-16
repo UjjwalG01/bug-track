717c569fed4f5513f01d436dda5caf0d3d8805ba
Ujjwal Giri <ug.online.st@gmail.com>
2023-10-16 14:28:40 +0545

    Radix UI themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
 import "@radix-ui/themes/styles.css";
+import "./theme-config.css";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
-import { Theme } from "@radix-ui/themes";
+import { Theme, ThemePanel } from "@radix-ui/themes";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
   title: "Track Your Bugs",
@@ -18,11 +22,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.variable}>
       <body className={inter.className}>
-        <Theme>
+        <Theme
+          appearance="light"
+          accentColor="jade"
+          grayColor="olive"
+          radius="large"
+        >
           <NavBar />
           <main className="p-8">{children}</main>
+          {/* <ThemePanel /> */}
         </Theme>
       </body>
     </html>
